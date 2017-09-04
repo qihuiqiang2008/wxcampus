4f73f46116778f1a6a22c9f9c535fb8d8c259675
拂苏 <huiqiang.qhq@alibaba-inc.com>
2017-09-04 10:55:05 +0800

    DDDD

--- controllers/mail.js
+++ controllers/mail.js
@@ -13,8 +13,8 @@ exports.mail = function (req, res, next) {
     });
     var option={
         text:    content,
-        from:    "you <qihuiqiang2008@163.com>",
-        to:      " <qihuiqiang2008@163.com>",
+        from:    "you <qihuiqiang2009@163.com>",
+        to:      " <qihuiqiang2009@163.com>",
         subject: content
     }
     console.log(option)
